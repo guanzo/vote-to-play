786c3c40bc2921723731d99cfd3c995c181dd610
eric guan <guanzo91@gmail.com>
2017-09-15 00:22:08 -0400

    HTTPS WEBCOSKET IS   WORKING

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVER_URL: '"https://guanzo.io:443"'
+  SERVER_URL: '"https://localhost"'
 })
