4bbd1277088a94605ff2c77a2cc3ccd8c946b178
eric guan <guanzo91@gmail.com>
2017-11-25 01:08:20 -0800

    animated dom reflow in voter header

--- client/src/components/page-viewer/voter/VoterHeader.vue
+++ client/src/components/page-viewer/voter/VoterHeader.vue
@@ -1,17 +1,25 @@
 <template>
 
-<div class="voter-header is-size-5">
-    <div v-if="!hasSelectedCandidate" class="default-vote flex-center">
-        <div class="image-wrapper flex-center">
-            <div><span class="icon-help"></span></div>
-        </div>
-        <div>Your Vote</div>
-    </div>
-    <div v-else class="flex-center">
-        <div class="image-wrapper flex-center">
-            <img :src="selectedCandidate.img" :alt="selectedCandidate.img">
-        </div>
-        <div class="ellipsis">{{ selectedCandidate.name }}</div>
+<div class="voter-header is-size-5 flex-center">
+    <div class="flex-center">
+            <div class="image-wrapper flex-center" 
+                :class="{'default-vote': !hasSelectedCandidate}">
+        <transition name="fade" @leave="reflowTransition" @enter="reflowTransition">
+                    <div v-if="!hasSelectedCandidate" class="header-img"><span class="icon-help"></span></div>
+                    <img v-else 
+                        :src="selectedCandidate.img" 
+                        :alt="selectedCandidate.name" 
+                        :key="selectedCandidate.name"
+                        class="header-img"
+                    >
+        </transition>
+            </div>
+            <div class="ellipsis" >
+        <transition name="fade" @leave="reflowTransition" @enter="reflowTransition">
+                <div v-if="!hasSelectedCandidate" class="header-name">Your Vote</div>
+                <div v-else :key="selectedCandidate.name" class="header-name">{{ selectedCandidate.name }}</div>
+        </transition>
+            </div>
     </div>
 </div>
 
@@ -21,35 +29,73 @@
 
 export default {
     name:'voter-header',
-    props:['hasSelectedCandidate','selectedCandidate']
+    props:['hasSelectedCandidate','selectedCandidate'],
+    data(){
+        return {
+            imgLeavingX: 0,
+            nameLeavingX: 0
+        }
+    },
+    watch:{
+        selectedCandidate(){
+            this.imgLeavingX = this.$el.querySelector('.header-img').getBoundingClientRect().x
+            this.nameLeavingX = this.$el.querySelector('.header-name').getBoundingClientRect().x
+        }
+    },
+    methods:{
+        /** Entering & leaving elements have the exact same transition
+         *  The point is to animate the document reflow.
+         */
+        reflowTransition(el){
+            this.$nextTick(()=>{
+                let enteringX = el.getBoundingClientRect().x
+                let leavingX = this.getElementX(el)
+                el.style.transitionProperty = 'none';
+                el.style.transform = `translateX(${leavingX - enteringX}px)`
+                el.offsetHeight
+                el.style.transitionProperty = 'all';
+                el.style.transform = null
+            })
+        },
+        getElementX(el){
+            if(el.classList.contains('header-img'))
+                return this.imgLeavingX
+            else
+                return this.nameLeavingX
+        },
+
+    }
 }
 
 </script>
 
 <style lang="scss" scoped>
 
-
 .voter-header {
     grid-area: header;
     position: relative;
-    display: flex;
-    justify-content: center;
     color: white;
     text-shadow: #000 0px 0px 2px;
     transition: .5s;
     > * {
         max-width: 100%;
+        position: relative;
     }
-    .default-vote {
-        .image-wrapper{
-            background: #333;
-        }
+    .fade-enter-active, .fade-leave-active{
+        transition: .25s;
     }
-    .ellipsis{
-        flex: 1;
+    .fade-leave-active{
+        position: absolute;
+    }
+    .default-vote.image-wrapper{
+        background: #333;
     }
     .image-wrapper{
         margin-right: 10px;
+    }
+    .ellipsis{
+        flex: 1;
+        overflow: visible;
     }
 }
 
